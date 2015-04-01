48d7d5a54019953afbc025ea17b7a181d260bbe1
kdesai12 <Kishan@Kishans-MacBook-Air.local>
2015-04-01 17:29:38 -0400

    Fixed LCD scrolling w/ timeouts, no Bugs.

--- NurseDeviceSpring/NurseDeviceSpring.ino
+++ NurseDeviceSpring/NurseDeviceSpring.ino
@@ -1,32 +1,29 @@
-#include <SPI.h>
+
 #include <Wire.h>
 #include <ST7565.h>
 #include <Button.h>
 
 //SETUP LCD DISPLAY
-int brightness = 200;
+byte brightness = 200;
 int rPin = 3;
 int gPin = 5;
 int bPin = 6;
 
-int vPin = 14;
+int postSetup = 0;
 //Previous Button Press Time
-long lastPress1 = 0;
-long lastPress2 = 0;
-
-//Last Vibrate Time
-long lastVibrate = 0;
-int vibTime = 500;
+int scrollPatientTime = 3;
+int lastScroll = 0;
 //initialize RGB values for backlight
 int r = 0;
 int g = 0;
 int b = 0;
 
 //Demo Data
-String nameQueue = "Albert,Bob,Carl123456789,Dick,Ed,";
-String roomQueue = "1,2,3,4,5,";
-String riskQueue = "3,4,3,4,4,";
-String timeQueue = "";
+String nameQueue = "Albert,Bob,";
+String roomQueue = "11,22,";
+String riskQueue = "3,4,";
+String timeQueue = "12,0,";
+
 // Initialize Data Queues
 //String nameQueue = "";
 //String roomQueue = "";
@@ -36,16 +33,16 @@ String timeQueue = "";
 // Initialize Single Patient Values
 String name = "";
 String rNum ="";
-int riskLevel = 0;
-float patTime=0;
+byte riskLevel = 0;
+int patTime=0;
 
-int currentDisplayItem = 1;
-int timeoutMins = 10; //Patient timeout Period Mins
+byte currentDisplayItem = 1;
+long lastPatientTIme = 0;
+byte timeoutSecs = 12; //Patient timeout Period secs
 long lastLCDUpdate = 0; // Last Time LCD was updated
-int lcdDelay = 750;; //Time btwn LCD update
+int lcdRefresh = 1250; //Time btwn LCD update
 // Patient  Timeout
-int timeoutInterval = 15000; //check if patients are timed out every XX ms
-long lastTimeoutCheck = 0;//last time patient timeout was checked
+int lastTimeoutCheck = 0; //last time patient timeout was checked
 
 //Intialize LCD ***
 // pin 12 - Serial data out (SID)
@@ -56,102 +53,161 @@ long lastTimeoutCheck = 0;//last time patient timeout was checked
 ST7565 lcd(12, 13, 9, 11, 10);
 
 void setup(void)
-{
+{                
+  Serial.begin(9600);
   // Set Background of LCD to white
   setOff();
   // Initialize background color pins
   pinMode(rPin, OUTPUT);
   pinMode(gPin, OUTPUT);
   pinMode(bPin, OUTPUT);
-  // Start vibration pin
-  pinMode(vPin, OUTPUT);
-  // Set Vibration pin off
-  digitalWrite(vPin, LOW);
-
 
   // Wait 2 seconds for device to start
   lcd.begin(0x1b);
-  delay(2000);
+  lcd.display();
+  delay(8000);
+  lcd.clear();
+  ram();
+  Serial.println(getQueueEntry(nameQueue,1));
+  delay(3000);
+  postSetup = secs();
 }
 
 void loop(void)
 {
-  if (queueSize() > 0) 
-  { lcdDelay = 750; }
-  else
-  { lcdDelay = 1250; }
-  
+  processData();
   updateDisplay();
-  // Check vibration status AND 
-  //turn off if vibraion has occured for > 750ms
-  if (millis() - lastVibrate > (long) vibTime)
-  { digitalWrite(vPin, LOW); }
 }
 
-//Functions
+void displayPatientInfo() {
+  // Displays and formats data to LCD Display
+  lcd.clear();
+  // When there are alerts to be shown
+  printQueues(String(queueSize()));
+  if (queueSize() >= 1) {
+    String S1 = "Name: " + name;
+    String S2 = "Room#: " + rNum;
+    String S3 = "Secs:";
+    S3 += String(secs());
+    S3 += "  # Alerts: " ;
+    S3 += String(queueSize());
+    char lcdLine1[S1.length()+1];
+    char lcdLine2[S2.length()+1];
+    char lcdQueueSize[S3.length()+1];
+    S1.toCharArray(lcdLine1, S1.length()+1);
+    S2.toCharArray(lcdLine2, S2.length()+1);
+    S3.toCharArray(lcdQueueSize, S3.length()+1);
+    lcd.drawstring(0, 0, lcdQueueSize);
+    lcd.drawstring(0, 1, lcdLine1);
+    lcd.drawstring(0, 2, lcdLine2);
+    lcd.display();
+  }
+  // When there are no alerts
+  else {
+    lcd.clear();
+    Serial.println("No Alerts");
+    char lcdLine1[18] = "No Patient Alerts";
+    lcd.drawstring(0, 1, lcdLine1);
+    lcd.display();
+    name = "";
+    rNum ="";
+    riskLevel = 1;
+    patTime=0;
+  }
+  // Set background based on riskLevel
+  switch (riskLevel)
+  {
+    //Taken Care of - GREEN
+  case 1:
+  brightness = 0;
+    setOff();
+    break;
+    //Nurse Acknowledged - BLUE
+  case 2:
+  brightness = 200;
+    setBlue();
+    break;
+    //Low Risk - YELLOW
+  case 3:
+  brightness = 250;
+    setYellow();
+    break;
+    //High Risk - RED
+  case 4:
+  brightness = 200;
+    setRed();
+    break;
+  }
+}
+
+void processData()
+{
+  if (queueSize() > 0)  {  lcdRefresh = 500;   }
+  else {   lcdRefresh = 1000;  }
+  
+  if ((secs() - getQueueEntry(timeQueue, currentDisplayItem).toInt() > timeoutSecs) && (queueSize() >= 1))
+      { 
+        Serial.println("Removing Patient " + String(currentDisplayItem));
+        removePatient(currentDisplayItem); 
+      }
+      
+  else if (secs() - lastScroll > scrollPatientTime) {lastScroll = secs();  currentDisplayItem++;  }
+  
+  if (currentDisplayItem > queueSize() && queueSize() > 0) 
+  {  currentDisplayItem = 1;  }
+  
+  if (queueSize() >= 1) {  getAllData(currentDisplayItem);  }
+}
+
+
+void updateDisplay() 
+// Update LCD display with proper patient data
+{
+  long begtime = millis();
+  // Scroll through multiple alerts every LCD REFRESH ms. 
+  if (millis() - lastLCDUpdate > lcdRefresh) {
+    // Show patient data on LCD
+    displayPatientInfo();
+    lastLCDUpdate = millis();
+    ram();
+  }
+}
+
+//void patientTimeout() {
+//  // 2000 ms Timeout check interval
+//  long begtime = millis();
+//  if (secs() - lastTimeoutCheck > 5) {
+//    Serial.println("Checking Timeout");
+//    for (int x = 1; x < queueSize(); x++ ){
+//      Serial.println(getQueueEntry(timeQueue, x));
+//      if (getQueueEntry(timeQueue, x).toInt() - secs() < timeoutSecs)
+//      { 
+//        Serial.println("Removing Patient " + String(x));
+//        removePatient(x); 
+//      }
+//    }
+//    lastTimeoutCheck = secs();
+//  }
+//  //Serial.println("Time to Timeout Patients = "+ String(millis() - begtime));
+//}
+
 void addPatient(String patientName, int patRoomNumber, int patRiskLevel) 
 //Add Patient Information to proper queues
 {
   nameQueue = addQueueEntry(nameQueue, patientName, 1);
   roomQueue = addQueueEntry(roomQueue, String(patRoomNumber), 1);
   riskQueue = addQueueEntry(riskQueue, String(patRiskLevel), 1);
-  timeQueue = addQueueEntry(timeQueue, String(float(millis()/1000)),1);
+  timeQueue = addQueueEntry(timeQueue, String(secs()),1);
 }
-
-void vibrate()
-//Vibrate every 3s
-{
-  if (millis() - lastVibrate > 2500) {
-  digitalWrite(vPin, HIGH);
-  lastVibrate = millis(); }
-}
-
-void updateDisplay() 
-// Update LCD display with proper patient data
-{
-  // Scroll through multiple alerts every LCD DELAY ms. 
-  if (millis() - lastLCDUpdate > lcdDelay) {
-  //Get patient data @ CURRENT DISPLAY ITEM index 
-  // Show patient data on LCD
-  displayPatientInfo(currentDisplayItem);
-  // Iterate through multiple alerts
-  currentDisplayItem++;
-  lastLCDUpdate = millis();
-}
-}
-
-void processData()
-{
-   patientTimeout();
-   if (currentDisplayItem > queueSize()) 
-   { 
-  // 
-      currentDisplayItem = 1; 
-   }
-
-   
-}
-  
 void removePatient(int itemNumber)
 // Remove patient from queue
 {
+  //printQueues("Patient " + String(itemNumber) + " Being Removed");
   nameQueue = deleteQueueEntry(nameQueue, itemNumber);
   roomQueue = deleteQueueEntry(roomQueue, itemNumber);
   riskQueue = deleteQueueEntry(riskQueue, itemNumber);
   timeQueue = deleteQueueEntry(timeQueue, itemNumber);
-}
-
-void patientTimeout()
-{
-  if (millis() - lastTimeoutCheck > (long)timeoutInterval) {
-  for (int x = 1; x < nameQueue.length(); x++ ){
-  if (getQueueEntry(timeQueue, x).toFloat() - (float)(millis()/1000) < (float)(-timeoutMins*60))
-  {
-    removePatient(x);
-  }
-  }
-  lastTimeoutCheck = millis();
-  }
+  //printQueues("Patient " + String(itemNumber) + " Removed");
 }
 
 void getAllData(int itemNumber)
@@ -160,7 +216,7 @@ void getAllData(int itemNumber)
   name = getQueueEntry(nameQueue, itemNumber);
   rNum = getQueueEntry(roomQueue, itemNumber);
   riskLevel = getQueueEntry(riskQueue, itemNumber).toInt();
-  patTime = getQueueEntry(timeQueue, itemNumber).toFloat();
+  patTime = getQueueEntry(timeQueue, itemNumber).toInt();
 }
 
 String deleteQueueEntry(String queue, int itemNumber)
@@ -168,17 +224,17 @@ String deleteQueueEntry(String queue, int itemNumber)
 { 
   if (itemNumber > 0)
   {
-  itemNumber--;
-  int count = 0;
-  int lastIndex = -1;
-  while (count < itemNumber) 
-  {
-    count++;
-    lastIndex = queue.indexOf(',',lastIndex+1); 
+    itemNumber--;
+    int count = 0;
+    int lastIndex = -1;
+    while (count < itemNumber) 
+    {
+      count++;
+      lastIndex = queue.indexOf(',',lastIndex+1); 
+    }
+    queue.remove(lastIndex+1, queue.indexOf(',',lastIndex+1) - lastIndex);
   }
-  queue.remove(lastIndex+1, queue.indexOf(',',lastIndex+1) - lastIndex);
-  }
-    return queue; // returns new queue after deleting item
+  return queue; // returns new queue after deleting item
 }
 
 //Complete
@@ -201,7 +257,7 @@ void changeRiskQueue(int itemNumber, char newRisk)
   itemNumber--;
   int count = 0;
   int lastIndex = 0;
-  
+
   while (count < itemNumber) 
   {
     count++;
@@ -218,106 +274,27 @@ String addQueueEntry(String queue, String entry, int itemNumber)
   int count = 0;
   int lastIndex = 0;
   while (count < itemNumber) 
-    {
-      count++;
-      lastIndex = queue.indexOf(',',lastIndex+1); 
-    }
-  if (itemNumber > 0) 
-    {
-      temp = queue.substring(0,lastIndex+1);
-      queue = temp + entry + "," + queue.substring(lastIndex+1, queue.length());
-    }
-  else if (itemNumber == 0) 
-    {
-      queue = entry + "," + queue;   
-    }
-  return queue;
-}
-
-String moveQueueEntry(String queue, int itemNumber, int newItemNumber)
-// Can be used to sort queues by moving items from itemNumber to new ItemNumber
-{
- String temp = getQueueEntry(queue, itemNumber);
- if (itemNumber > newItemNumber) 
- {
- queue = addQueueEntry(queue,temp, newItemNumber);
- queue = deleteQueueEntry(queue, newItemNumber);
- queue = deleteQueueEntry(queue, itemNumber+1);
- }
- if (itemNumber < newItemNumber) 
- {
- queue = deleteQueueEntry(queue, itemNumber);
- queue = addQueueEntry(queue,temp, newItemNumber);
- queue = deleteQueueEntry(queue, newItemNumber);
- }
- return queue;
-}
-
-int queueSize() {
-  //Finds number of names in  nameQueue 
-  int count = 0; 
-  for (int x = 1; x < nameQueue.length(); x++) {
-    if (nameQueue.charAt(x) == ',')
-    {
-    count++;
-    }
-  }
-  return count;
-}
-// ***
-void displayPatientInfo(int itemNumber) {
-  // Displays and formats data to LCD Display
- lcd.clear();
- getAllData(currentDisplayItem);
-
- // When there are alerts to be shown
- if (queueSize() >= 1) {
-   String S1 = "Name: " + name;
-   String S2 = "Room#: " + rNum;
-    char lcdLine1[S1.length()];
-    char lcdLine2[S2.length()];
-    S1.toCharArray(lcdLine1,S1.length());
-    S2.toCharArray(lcdLine2,S2.length());
-    vibrate();
-    lcd.drawstring(0, 1, lcdLine1);
-    lcd.drawstring(0, 2, lcdLine2);
- }
-// When there are no alerts
- else {
-  char lcdLine1[] = "No Patient Alerts";
-  lcd.drawstring(0, 1, lcdLine1);
-  setOff();
-  }
-  // Print Queue Size
-  lcd.drawchar(100,0,char(queueSize()));
-  
-  // Set background based on riskLevel
-  switch (riskLevel) 
   {
-  //Taken Care of - GREEN
-  case 1:
-  setGreen(); 
-  break;
-  //Nurse Acknowledged - BLUE
-  case 2:
-  setBlue();
-  break;
-  //Low Risk - YELLOW
-  case 3:
-  setYellow();
-  break;
-  //High Risk - RED
-  case 4: 
-  setRed();
-  break;
+    count++;
+    lastIndex = queue.indexOf(',',lastIndex+1); 
   }
+  if (itemNumber > 0) 
+  {
+    temp = queue.substring(0,lastIndex+1);
+    queue = temp + entry + "," + queue.substring(lastIndex+1, queue.length());
+  }
+  else if (itemNumber == 0) 
+  {
+    queue = entry + "," + queue;   
+  }
+  return queue;
 }
 
 void setOff()
 {
-analogWrite(rPin, 0);
-analogWrite(gPin, 0);
-analogWrite(bPin, 0);
+  analogWrite(rPin, 0);
+  analogWrite(gPin, 0);
+  analogWrite(bPin, 0);
 }
 
 void setRed()
@@ -342,23 +319,23 @@ void setGreen()
 //Complete
 void setBG(int r, int g, int b, int brightness) 
 {
-//Map to account for increased brightness of RED led
-r = map(r, 0, 255, 0, 200);
-g = map(g, 0, 255, 0, 200);
+  //Map to account for increased brightness of RED led
+  r = map(r, 0, 255, 0, 200);
+  g = map(g, 0, 255, 0, 200);
 
-// Map to achieve correct display brightness
-r = map(r, 0, 255, 0, brightness);
-g = map(g, 0, 255, 0, brightness);
-b = map(b, 0, 255, 0, brightness);
+  // Map to achieve correct display brightness
+  r = map(r, 0, 255, 0, brightness);
+  g = map(g, 0, 255, 0, brightness);
+  b = map(b, 0, 255, 0, brightness);
 
-// Invert RGB values from 0-255 to 255-0 due to display feature (LCD dependent)
-r = map(r, 0, 255, 255, 0);
-g = map(g, 0, 255, 255, 0);
-b = map(b, 0, 255, 255, 0);
-// Output proper RGB values for BG color
-analogWrite(rPin, r);
-analogWrite(gPin, g);
-analogWrite(bPin, b);
+  // Invert RGB values from 0-255 to 255-0 due to display feature (LCD dependent)
+  r = map(r, 0, 255, 255, 0);
+  g = map(g, 0, 255, 255, 0);
+  b = map(b, 0, 255, 255, 0);
+  // Output proper RGB values for BG color
+  analogWrite(rPin, r);
+  analogWrite(gPin, g);
+  analogWrite(bPin, b);
 }
 
 int existsPatient(int room)
@@ -368,19 +345,17 @@ int existsPatient(int room)
   int exPat = 0;
   while (i <= queueSize() && exPat == 0)
   {
-    if (getQueueEntry(roomQueue, i).equals(String( room)))
-   {
-       exPat = 1;
-   }
-   i++;
+    if (getQueueEntry(roomQueue, i).equals(String(room))){
+      exPat = 1; 
+    }
+    i++;
   }
- return exPat;
+  return exPat;
 }
 
 int findRoomIndex(int room )
 // Find index of patient in queue
 {
-  printQueues("Finding Patient ---> " + String(room));
   int index = 0;
   for (int i = 1; i <= queueSize(); i++) {
     if (room == getQueueEntry(roomQueue,i).toInt())
@@ -388,24 +363,36 @@ int findRoomIndex(int room )
       index = i;
     }
   }
-  Serial.print("Found! ---> Index of " + String(room)); Serial.println(index);
   return index;
 }
 
-// this handy function will return the number of bytes currently free in RAM, great for debugging!   
 int freeRam(void)
 {
-  extern int  __bss_end; 
-  extern int  *__brkval; 
-  int free_memory; 
-  if((int)__brkval == 0) {
-    free_memory = ((int)&free_memory) - ((int)&__bss_end); 
+  extern int  __bss_end;
+  extern int  *__brkval;
+  int free_memory;
+  if ((int)__brkval == 0) {
+    free_memory = ((int)&free_memory) - ((int)&__bss_end);
   }
   else {
-    free_memory = ((int)&free_memory) - ((int)__brkval); 
+    free_memory = ((int)&free_memory) - ((int)__brkval);
   }
-  return free_memory; 
-} 
+  return free_memory;
+}
+
+void ram() { Serial.println("Free Ram --> " + String(freeRam()));  }
+
+int queueSize() {
+  //Finds number of names in  nameQueue 
+  int count = 0; 
+  for (int x = 1; x < nameQueue.length(); x++) {
+    if (nameQueue.charAt(x) == ',')
+    {
+      count++;
+    }
+  }
+  return count;
+}
 
 void printQueues(String Header)
 {
@@ -413,4 +400,27 @@ void printQueues(String Header)
   Serial.println(nameQueue);
   Serial.println(roomQueue);
   Serial.println(riskQueue);
+  Serial.println(timeQueue);
 }
+
+int secs() { return (int)(millis()/1000) - postSetup; }
+
+String moveQueueEntry(String queue, int itemNumber, int newItemNumber)
+// Can be used to sort queues by moving items from itemNumber to new ItemNumber
+{
+  String temp = getQueueEntry(queue, itemNumber);
+  if (itemNumber > newItemNumber) 
+  {
+    queue = addQueueEntry(queue,temp, newItemNumber);
+    queue = deleteQueueEntry(queue, newItemNumber);
+    queue = deleteQueueEntry(queue, itemNumber+1);
+  }
+  if (itemNumber < newItemNumber) 
+  {
+    queue = deleteQueueEntry(queue, itemNumber);
+    queue = addQueueEntry(queue,temp, newItemNumber);
+    queue = deleteQueueEntry(queue, newItemNumber);
+  }
+  return queue;
+}
+
--- NurseLCDTestSpring/NurseLCDTestSpring.ino
+++ NurseLCDTestSpring/NurseLCDTestSpring.ino
@@ -1,5 +1,4 @@
 #include "ST7565.h"
-#include "U8glib.h"
 
 // the LCD backlight is connected up to a pin so you can turn it on & off
 int rPin = 3;
@@ -17,79 +16,144 @@ int b = 0;
 // pin 9 - Data/Command select (RS or A0)
 // pin 11 - LCD reset (RST)
 // pin 10 - LCD chip select (CS)
-ST7565 glcd(12, 13, 9, 11, 10);
+ST7565 lcd(12, 13, 9, 11, 10);
 
+String name = "Bob";
+String rNum = "42";
+int riskLevel = 3;
 // The setup() method runs once, when the sketch starts
-void setup()   {                
+void setup()   {
   Serial.begin(9600);
-delay(5000); 
-  Serial.print(freeRam());
-  
+  delay(7000);
+  Serial.println(freeRam());
+
   // turn on backlight
-    // Initialize background color pins
+  // Initialize background color pins
   pinMode(rPin, OUTPUT);
   pinMode(gPin, OUTPUT);
   pinMode(bPin, OUTPUT);
   setGreen();
   // initialize and set the contrast to 0x18
-  glcd.begin(0x1b);
-  Serial.print(freeRam());
-  glcd.clear();
-
-  // draw a string at location (0,0)
-  String lcd1 = "1234567890abcdefghijk";
-  char message[16];
-  lcd1.toCharArray(message,16);
-  glcd.drawstring(0, 0,message );
-  glcd.display();
+  lcd.begin(0x1b);
+  Serial.println(freeRam());
+  lcd.clear();
   delay(2000);
-  glcd.clear();
+  displayPatientInfo();
+  delay(2000);
+}
 
-    Serial.print(freeRam());
-    delay(20000);
+void displayPatientInfo() {
+  // Displays and formats data to LCD Display
+  lcd.clear_display();
+
+  Serial.println(freeRam());
+  // When there are alerts to be shown
+  //if (queueSize() >= 1) {
+  String S1 = "Name: " + name;
+  String S2 = "Room#: " + rNum;
+  String S3 = String(1);
+  char lcdLine1[S1.length()+1];
+  char lcdLine2[S2.length()+1];
+  char lcdQueueSize[S3.length()+1];
+  S1.toCharArray(lcdLine1, S1.length()+1);
+  S2.toCharArray(lcdLine2, S2.length()+1);
+  S3.toCharArray(lcdQueueSize, S3.length()+1);
+  lcd.drawstring(114, 0, lcdQueueSize);
+  lcd.drawstring(0, 1, lcdLine1);
+  lcd.drawstring(0, 2, lcdLine2);
+  Serial.println(freeRam());
+  lcd.display();
+
+  //}
+
+  // When there are no alerts
+   else {
+    char lcdLine1[] = "No Patient Alerts";
+    lcd.drawstring(0, 1, lcdLine1);
+    setOff();
+  }
+  // Print Queue Size
+  lcd.drawchar(114,0,'0');
+
+  // Set background based on riskLevel
+  switch (riskLevel)
+  {
+    //Taken Care of - GREEN
+    case 1:
+      setGreen();
+      break;
+    //Nurse Acknowledged - BLUE
+    case 2:
+      setBlue();
+      break;
+    //Low Risk - YELLOW
+    case 3:
+      setYellow();
+      break;
+    //High Risk - RED
+    case 4:
+      setRed();
+      break;
+  }
 }
 
 
-void loop()                     
+void loop()
 {
 
 }
 
-// this handy function will return the number of bytes currently free in RAM, great for debugging!   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// this handy function will return the number of bytes currently free in RAM, great for debugging!
 int freeRam(void)
 {
-  extern int  __bss_end; 
-  extern int  *__brkval; 
-  int free_memory; 
-  if((int)__brkval == 0) {
-    free_memory = ((int)&free_memory) - ((int)&__bss_end); 
+  extern int  __bss_end;
+  extern int  *__brkval;
+  int free_memory;
+  if ((int)__brkval == 0) {
+    free_memory = ((int)&free_memory) - ((int)&__bss_end);
   }
   else {
-    free_memory = ((int)&free_memory) - ((int)__brkval); 
+    free_memory = ((int)&free_memory) - ((int)__brkval);
   }
-  return free_memory; 
-} 
+  return free_memory;
+}
 
-void setBG(int r, int g, int b, int brightness) 
+void setBG(int r, int g, int b, int brightness)
 {
-//Map to account for increased brightness of RED led
-r = map(r, 0, 255, 0, 200);
-g = map(g, 0, 255, 0, 200);
+  //Map to account for increased brightness of RED led
+  r = map(r, 0, 255, 0, 200);
+  g = map(g, 0, 255, 0, 200);
 
-// Map to achieve correct display brightness
-r = map(r, 0, 255, 0, brightness);
-g = map(g, 0, 255, 0, brightness);
-b = map(b, 0, 255, 0, brightness);
+  // Map to achieve correct display brightness
+  r = map(r, 0, 255, 0, brightness);
+  g = map(g, 0, 255, 0, brightness);
+  b = map(b, 0, 255, 0, brightness);
 
-// Invert RGB values from 0-255 to 255-0 due to display feature (LCD dependent)
-r = map(r, 0, 255, 255, 0);
-g = map(g, 0, 255, 255, 0);
-b = map(b, 0, 255, 255, 0);
+  // Invert RGB values from 0-255 to 255-0 due to display feature (LCD dependent)
+  r = map(r, 0, 255, 255, 0);
+  g = map(g, 0, 255, 255, 0);
+  b = map(b, 0, 255, 255, 0);
 
-// Output proper RGB values for BG color
-analogWrite(rPin, r);
-analogWrite(gPin, g);
-analogWrite(bPin, b);
+  // Output proper RGB values for BG color
+  analogWrite(rPin, r);
+  analogWrite(gPin, g);
+  analogWrite(bPin, b);
 }
 
 void setRed()
@@ -99,7 +163,7 @@ void setRed()
 
 void setYellow()
 {
-  setBG(255, 200, 5, brightness);
+  setBG(255, 255, 5, brightness);
 }
 
 void setBlue()
